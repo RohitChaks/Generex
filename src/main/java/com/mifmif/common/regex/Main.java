6d0951739af4d17c74781d6ee3e81bdd67feb5d9
RohitChaks <rohitashwachaks@gmail.com>
2017-08-21 03:04:41 +0530

    Update Main.java
    This allows the user to generate language from his own supplied regex given in the input file.

--- src/main/java/com/mifmif/common/regex/Main.java
+++ src/main/java/com/mifmif/common/regex/Main.java
@@ -15,43 +15,59 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.mifmif.common.regex;
 
-//import java.util.List;
+package regex;
 
-import com.mifmif.common.regex.util.Iterator;
+import java.io.*;
+import regex.GenerexIterator;
 
-/**
- * @author y.mifrah
- *
- */
-public class Main {
-	public static void main(String[] args) {
-		Generex generex = new Generex("[0-3]([a-c]|[e-g]{1,2})");
 
-		// generate the second String in lexicographical order that match the
-		// given Regex.
-		String secondString = generex.getMatchedString(2);
-		System.out.println(secondString);// it print '0b'
+public class Main
+{
+	public static void main(String[] args)
+	{	
+	    try
+	    {
+			FileInputStream in = new FileInputStream(args[0]);
+			FileOutputStream ou = new FileOutputStream(args[1],true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 
-		// Generate all String that matches the given Regex.
-		//List<String> matchedStrs = generex.getAllMatchedStrings();
+	    	
+	    	
+	    	while (in.read()!=0)
+	    	{
 
-		// Using Generex iterator
-		Iterator iterator = generex.iterator();
-		while (iterator.hasNext()) {
-			System.out.print(iterator.next() + " ");
-		}
-		// it prints:
-		// 0a 0b 0c 0e 0ee 0ef 0eg 0f 0fe 0ff 0fg 0g 0ge 0gf 0gg
-		// 1a 1b 1c 1e 1ee 1ef 1eg 1f 1fe 1ff 1fg 1g 1ge 1gf 1gg
-		// 2a 2b 2c 2e 2ee 2ef 2eg 2f 2fe 2ff 2fg 2g 2ge 2gf 2gg
-		// 3a 3b 3c 3e 3ee 3ef 3eg 3f 3fe 3ff 3fg 3g 3ge 3gf 3gg
+	    		String ip = br.readLine();
+	    		System.out.println(ip);
 
-		// Generate random String
-		String randomStr = generex.random();
-		System.out.println(randomStr);// a random value from the previous String
-										// list
-
+	    		Generex generex = new Generex(ip);
+	    		
+	    		// Using Generex iterator
+	    		GenerexIterator iterator = (GenerexIterator) generex.iterator();
+	    		
+	    		//Printing output with desired formatting.		
+	    		int i=0;
+	    		while(iterator.hasNext())
+	    		{
+	    			String op = iterator.next();
+	    			System.out.println(op);
+	    			byte b[] = op.getBytes();
+	    			ou.write(b);
+	    			ou.write("\n".getBytes());
+	    		}
+	    	}
+	    	if (in != null)
+	    	{
+	    		in.close();
+	    	}
+	    	if (ou != null)
+	    	{
+	    		ou.close();
+	    	}
+	    }
+	    catch(IOException e)
+	    {
+	    	System.err.println("asd");
+	    }
 	}
 }
